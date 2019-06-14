f65a119820cce68a888dfdb5dfd032fe0b3049d3
Dhiman Sarkar <dhiman.eminem@gmail.com>
2019-06-15 02:03:05 +0530

    Member Details

--- Light Of Science Membership Database/member_details.py
+++ Light Of Science Membership Database/member_details.py
@@ -1,5 +1,31 @@
 def member_details():
-    FirstName = input("Enter First Name :: ")
-    LastName =  input("Enter Last Name  :: ")
-    member = (FirstName, LastName)
-    return member
+    BSSID=input("BSS Membership ID :: ")
+
+    print ("\n")
+    FirstName = input("First Name :: ")
+    LastName =  input("Last Name  :: ")
+
+    print ("\n")
+    Sex = input("Sex :: ")
+    DOB = input("Data of Birth (DD/MM/YYYY) :: ") 
+
+    print ("\n")
+    Address =   input("Address     :: ")
+    District =  input("District    :: ")
+    PostalCode =input("Postal Code :: ")
+
+    print ("\n")
+    ContactNumber=[input("Primary Contact Number :: "), input("Optional Contact Number :: ")]
+    Email = input("Email :: ")
+
+    print ("\n")
+    Profession = input("Profession :: ")
+    Designation = input ("Designation :: ")
+    InstituteOffice = input("Institute/Office name :: ")
+
+    print ("\n")
+    DateOfSubmission = input("Date of Submission (DD/MM/YYYY) :: ")
+    Reciever = input("Recieved by, ")
+
+    MemberDetails = (BSSID, FirstName, LastName, Sex, DOB, Address, District, PostalCode, ContactNumber, Email, Profession, Designation, InstituteOffice, DateOfSubmission, Reciever)
+    return MemberDetails
